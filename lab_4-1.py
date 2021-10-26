d4c470f25991a97b6073aaac6348addc6255a52a
p22mbonilla <p22mbonilla@fairfieldprep.org>
2021-10-26 10:11:52 -0400

    add file

---
+++ lab_4-1.py
@@ -0,0 +1,12 @@
+# Author MB 10/26/2021
+
+print('Fairfield' + 'Prep')
+
+print('-' * 20)
+
+s = "I loving this short story Im reading, \"The Fall of the House of Usher\""
+print(s)
+print(len(s))
+
+a = "apple"
+print(a[3])
