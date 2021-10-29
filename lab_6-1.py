ddec36bcf002e35a0858f48b8fa6e5f525d11b6f
p22mbonilla <p22mbonilla@fairfieldprep.org>
2021-10-29 10:45:15 -0400

    add file

---
+++ lab_6-1.py
@@ -0,0 +1,7 @@
+# Author MB 10/29/2021
+
+weird = "flibbertigibbet"
+print(weird.find("t"))
+
+senten = "I wish, I wish, I was a fish."
+print(senten.split(","))
---
+++ lab_6-2.py
@@ -0,0 +1,8 @@
+# Author MB 10/29/2021
+
+first = input("what is player 1 first name: ")
+second = input("what is player 2 first name: ")
+
+fs = "player 1 {0}, player 2 {1}, are ready to play.".format(first, second)
+
+print(fs)
